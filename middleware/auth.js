d459f86ee03cb5e8e1476c3917668a5cdda4b2cd
Mikk <mikk.villem@gmail.com>
2020-08-04 15:42:45 +0300

    Add ENV variables

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,24 @@
-const config = process.env.jwtSecret; //require('config'); config.get('jwtSecret')
-const jwt = require('jsonwebtoken');
+const config = process.env.JWTSECRET; //require('config'); config.get('jwtSecret')
+const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-    const token = req.header('x-auth-token');
+  const token = req.header("x-auth-token");
 
-    //Check for token
-    if(!token) {
-       return res.status(401).json({ msg: "No token, authorization denied"});
-    }
+  //Check for token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    try {
-        //Verify token
-        const decoded = jwt.verify(token, config);
+  try {
+    //Verify token
+    const decoded = jwt.verify(token, config);
 
-        //Add user from payload
-        req.user = decoded;
-        next();
-    } catch(e) {
-        res.status(400).json({msg: "Token is not valid!"})
-    }
-
+    //Add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: "Token is not valid!" });
+  }
 }
 
 module.exports = auth;
